1e1c8beeb37f2443e670ec3968af9f5939e73504
Kattya94 <kattya.cuevas@gmail.com>
2013-11-20 23:41:50 -0500

    Interfaz lista (creo)

--- analizador.py
+++ analizador.py
@@ -3,17 +3,15 @@ from Tkinter import *  #Importamos el módulo para la interfaz
 
 ventana = Tk() #creamos la ventana principal
 ventana.title('Analizador Léxico')
-ventana.geometry("500x500") #tamaño de la ventana
-label = Label(ventana, text="		")
-label.grid(row=1,column=1)
+ventana.geometry("600x500") #tamaño de la ventana
 label1 = Label(ventana, text="Ingresa la cadena:")
-label1.grid(row=1,column=2)
-label2 = Label(ventana, text="		")
-label2.grid(row=1,column=3)
-label3 = Label(ventana, text="Tokens encontrados")
-label3.grid(row=1,column=4)
-#text1 = Text(ventana)
-#text1.grid(row=2,column=1-2)
+label1.grid(row=1,column=1)
+label2 = Label(ventana, text="Tokens encontrados")
+label2.grid(row=1,column=2)
+text1 = Text(ventana,width=40,height=4)
+text1.grid(row=2,column=1)
+text2 = Text(ventana,width=40,height=4)
+text2.grid(row=2,column=2)
 boton = Button(ventana, text="Procesa")
-boton.grid(row=4,column=4)
+boton.grid(row=4,column=1)
 ventana.mainloop() #evento que llama al inicio del programa
